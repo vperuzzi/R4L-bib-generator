9106c5811124f08193551d3ecca10882a5e0622a
Tia Peruzzi <tiamariaperuzzi@gmail.com>
2020-05-15 13:02:21 -0400

    Add Certificate Generation

--- js/script.js
+++ js/script.js
@@ -1,31 +1,52 @@
-var canvas = document.getElementById('imageCanvas');
-var ctx = canvas.getContext('2d');
+var bibCanvas = document.getElementById('bibCanvas');
+var bibctx = bibCanvas.getContext('2d');
+var certCanvas = document.getElementById('certCanvas');
+var certctx = certCanvas.getContext('2d');
 var img = new Image();
-
+var img2 = new Image();
 
 document.getElementById('bibGen').onclick = function() {
   doBib();
+  doCert();
 };
 
 function init() {
   img.onload = function() {
-    canvas.width = img.width;
-    canvas.height = img.height;
-    ctx.drawImage(img, 0, 0);
+    bibCanvas.width = img.width;
+    bibCanvas.height = img.height;
+    bibctx.drawImage(img, 0, 0);
   };
   img.src = "images/bib.jpg";
+
+  img2.onload = function() {
+    certCanvas.width = img2.width;
+    certCanvas.height = img2.height;
+    certctx.drawImage(img2, 0, 0);
+  };
+  img2.src = "images/certificate.jpg";
 }
 
 function doBib() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(img, 0, 0);
+  bibctx.clearRect(0, 0, bibCanvas.width, bibCanvas.height);
+  bibctx.drawImage(img, 0, 0);
   var number = Math.floor((Math.random() * 90000) + 10000);
-  var name = document.getElementById("name").value;
-  ctx.font = '50px sans-serif';
-  ctx.textAlign = 'center';
-  ctx.fillText(name.toUpperCase(), canvas.width / 2, canvas.height / 2 - 100);
-  ctx.font = '300px sans-serif';
-  ctx.fillText(number.toString(), canvas.width / 2, canvas.height / 2 + 175);
+  var name = document.getElementById("last_name").value;
+  bibctx.font = '50px sans-serif';
+  bibctx.textAlign = 'center';
+  bibctx.fillText(name.toUpperCase(), bibCanvas.width / 2, bibCanvas.height / 2 - 85);
+  bibctx.font = '300px sans-serif';
+  bibctx.fillText(number.toString(), bibCanvas.width / 2, bibCanvas.height / 2 + 175);
+}
+
+function doCert() {
+  certctx.clearRect(0,0, certCanvas.width, certCanvas.height);
+  certctx.drawImage(img2, 0, 0);
+  var first = document.getElementById("first_name").value;
+  var last = document.getElementById("last_name").value;
+  certctx.font = "100px sans-serif";
+  certctx.textAlign = 'center';
+  certctx.fillText(first.toUpperCase(), certCanvas.width/2, certCanvas.height/2 + 75);
+  certctx.fillText(last.toUpperCase(), certCanvas.width/2, certCanvas.height/2 + 175);
 }
 
 init();
